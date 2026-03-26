a351d27828912e3ffc5408b03954e974aef61f34
Soumil Tiwary <soumil.tiwary@gmail.com>
2026-03-26 11:32:30 +0530

    Implement conversation history in run_pipeline
    Added conversation history management to the pipeline, allowing the assistant to reference the last two interactions for context.

--- pipeline.py
+++ pipeline.py
@@ -1,16 +1,35 @@
 from agent import planning_agent,retrieval_agent,synthesis_agent
 from agent import load_vectorstore,get_llm
 
+vectorstore=load_vectorstore()
+llm=get_llm()
+conversation_history = []
 def run_pipeline(query):
     print("\n"+"="*50)
     print(f"Query: {query}")
     print("="*50)
-    vectorstore=load_vectorstore()
-    llm=get_llm()
+    if conversation_history:
+        history_text = "\n".join([
+            f"User: {h['query']}\nAssistant: {h['answer']}"
+            for h in conversation_history[-2:]  # only last 2 chats will be stored...
+        ])
+        enriched_query = f"""Previous conversation:
+{history_text}
 
-    sub_questions=planning_agent(query,llm)
+Current question: {query}
+
+Answer the current question, using the conversation above only if it is relevant."""
+    else:
+        enriched_query = query
+    sub_questions=planning_agent(enriched_query,llm)
     retrieval_result=retrieval_agent(sub_questions,vectorstore,llm)
-    final_result=synthesis_agent(query,retrieval_result,llm)
+    final_result=synthesis_agent(enriched_query,retrieval_result,llm)#since its history now query has to change to enriched query
+    conversation_history.append({
+        "query": query,
+        "answer": final_result["answer"]
+    })
+    if len(conversation_history) > 2:#not letting the history extend more than 2.
+        conversation_history.pop(0)
     print("\n"+"="*50)
     print("FINAL ANSWER:")
     print(final_result["answer"])
@@ -21,5 +40,8 @@ def run_pipeline(query):
 
     return final_result
 if __name__=="__main__":
-    query=input("Enter your question:")
-    run_pipeline(query)
+    while True:
+        query=input("\n Enter your question(or exit to quit):")
+        if query.lower()=="exit":
+            break
+        run_pipeline(query)
